1827da020483364c3c0cb5afff1f34241766e322
Alireza Bashiri <al3rez@gmail.com>
2023-02-04 18:39:05 +0700

    feat: override model generator to use tabs

--- lib/generators/model_generator.rb
+++
@@ -1,7 +0,0 @@
-class ModelGenerator < Rails::Generators::NamedBase
-    source_root Rails::Generators::ModelGenerator.source_root
-
-    def fix_indentation
-        gsub_file 'app/models/'+file_name+'.rb', /  /, "    "
-    end
-end
